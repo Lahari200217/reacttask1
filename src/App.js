cbe6fd68b5b1fc27a3e3d20ce8169e8eeed76717
Lahari200217 <takkasilalahari@gmail.com>
2023-10-14 18:05:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Count from './components/count';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Count></Count>
     </div>
   );
 }
 
 export default App;
+
+
+// echo "# reacttask1" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Lahari200217/reacttask1.git
+// git push -u origin main
