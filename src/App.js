c7b3e3fb8b6b7c4a81065d933012cd9a750fc32f
nithin988 <nithinakireddy112@gmail.com>
2023-10-15 13:23:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,10 +15,3 @@ export default App;
 
 
 
-// echo "# react-task1" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/nithin988/react-task1.git
-// git push -u origin main
